67c5a60dbb04887bb58b3f769e139e6a4661d05f
Sathvik <sathvikmurthy31@gmail.com>
2021-08-23 15:48:58 +0530

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
 import Layout from "../components/Layout";
 import { RecoilRoot } from 'recoil';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 
 function MyApp({ Component, pageProps }) {
-  return (
+
+	const router = useRouter()
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
+  	return (
   		<RecoilRoot>
 	  		<Layout>
 	  			<Component {...pageProps} />
